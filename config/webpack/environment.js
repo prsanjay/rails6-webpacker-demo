9c78de25402a0a086e3cfbe7da5977f23457494e
Ross Kaffenberger <rosskaff@gmail.com>
2019-12-16 20:50:21 -0500

    Add pikaday demo with ProvidePlugin configuration

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,9 @@
 const { environment } = require('@rails/webpacker')
+const webpack = require('webpack')
+const {resolve} = require('path');
+
+environment.plugins.append('Provide', new webpack.ProvidePlugin({
+  Datepicker: [resolve('app/javascript/application/datepicker'), 'default']
+}))
 
 module.exports = environment

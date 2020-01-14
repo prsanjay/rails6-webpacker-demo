14c4b9fbbdbb65967c9aa04b784b4ae17e180538
Ross Kaffenberger <rosskaff@gmail.com>
2020-01-13 19:00:37 -0500

    Add leaflet example with legacy plugin
    Answers https://stackoverflow.com/questions/59670743/leaflet-with-webpack-in-rails-6-l-timeline-is-not-a-function

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,24 @@
 const { environment } = require('@rails/webpacker')
+const webpack = require('webpack')
+
+environment.plugins.append('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    jquery: 'jquery',
+    // 'window.Jquery': 'jquery',
+    Popper: ['popper.js' ,'default'],
+    L: 'leaflet' // didn't help
+  }))
+
+
+environment.splitChunks()
+
+environment.plugins.append(
+  'lodash',
+  new webpack.ProvidePlugin({
+    _: 'lodash',
+  })
+)
 
 module.exports = environment
